d8c5c7d037615f0208b3d2d9e62ce2853b2dea65
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-07-21 16:56:58 +0530

    update

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,22 +1,24 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import GlobalRoutes from "./index";
-import ComingSoon from "../pages/comingSoon/ComingSoon";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import GlobalRoutes from './index';
+import ComingSoon from '../pages/comingSoon/ComingSoon';
 
 export default function Routing(props) {
   return (
-    <>
-      {GlobalRoutes().map((route) => {
-        return (
-          <Routes>
-            <Route {...props} path={route.path} exact element={route.element} />
-          </Routes>
-        );
-      })}
-      <Routes>
-         {/* Catch-all route */}
-         <Route path="*" element={<ComingSoon />} />
-      </Routes>
-    </>
+    <Routes>
+      {GlobalRoutes().map(route => (
+        <Route
+          {...props}
+          path={route.path}
+          exact
+          element={route.element}
+        />
+      ))}
+      {/* Catch-all route */}
+      <Route
+        path='*'
+        element={<ComingSoon />}
+      />
+    </Routes>
   );
 }
